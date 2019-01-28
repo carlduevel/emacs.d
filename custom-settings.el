e7609d1e3ab3987761ded0445f539d5ba5eeff43
Carl Düvel <c.a.duevel@gmail.com>
2019-01-28 21:33:17 +0100

    Switch custom file

--- custom-settings.el
+++ custom-settings.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (exec-path-from-shell evil-escape evil))))
+ '(package-selected-packages
+   (quote
+    (org-gcal oauth2 org-ref org-pomodoro org-caldav magit zenburn-theme writeroom-mode org-bullets writegood-mode popup synosaurus which-key evil-leader exec-path-from-shell evil-escape evil)))
+ '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
