d9154f12466c4d19dc9eb26742ebca8673d4117c
Carl Düvel <c.a.duevel@gmail.com>
2019-02-04 20:35:56 +0100

    Add auctex

--- custom-settings.el
+++ custom-settings.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(debug-on-error t)
  '(package-selected-packages
    (quote
-    (projectile org-gcal oauth2 org-ref org-pomodoro org-caldav magit zenburn-theme writeroom-mode org-bullets writegood-mode popup synosaurus which-key evil-leader exec-path-from-shell evil-escape evil)))
+    (auctex docker-compose-mode projectile org-gcal oauth2 org-ref org-pomodoro org-caldav magit zenburn-theme writeroom-mode org-bullets writegood-mode popup synosaurus which-key evil-leader exec-path-from-shell evil-escape evil)))
  '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
