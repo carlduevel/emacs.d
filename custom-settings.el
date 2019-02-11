5a5a9cba225fca6492baac4c62060a088df7474a
Carl Düvel <c.a.duevel@gmail.com>
2019-02-11 11:23:44 +0100

    Add docker modes

--- custom-settings.el
+++ custom-settings.el
@@ -6,7 +6,7 @@
  '(debug-on-error t)
  '(package-selected-packages
    (quote
-    (ag auctex docker-compose-mode projectile org-gcal oauth2 org-ref org-pomodoro org-caldav magit zenburn-theme writeroom-mode org-bullets writegood-mode popup synosaurus which-key evil-leader exec-path-from-shell evil-escape evil)))
+    (atomic-chrome dockerfile-mode ag auctex docker-compose-mode projectile org-gcal oauth2 org-ref org-pomodoro org-caldav magit zenburn-theme writeroom-mode org-bullets writegood-mode popup synosaurus which-key evil-leader exec-path-from-shell evil-escape evil)))
  '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
